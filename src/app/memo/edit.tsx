9f1e7d1df77f5c4aafc0bd983715190323795606
shogo4131 <shougo08697118@gmail.com>
2025-02-02 13:04:54 +0900

    feat: memo編集画面のui作成

---
+++ src/app/memo/edit.tsx
@@ -0,0 +1,40 @@
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  KeyboardAvoidingView,
+} from "react-native";
+import { Header } from "../../components/Header";
+import { CircleButton } from "../../components/CircleButton";
+import { Feather } from "@expo/vector-icons";
+
+export default function EditMemo() {
+  return (
+    <KeyboardAvoidingView behavior="height" style={styles.container}>
+      <Header />
+      <View style={styles.inputContainer}>
+        <TextInput multiline style={styles.input} value="買い物リスト" />
+      </View>
+      <CircleButton>
+        <Feather name="check" size={24} color="white" />
+      </CircleButton>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingVertical: 32,
+  },
+  input: {
+    flex: 1,
+    textAlignVertical: "top",
+    fontSize: 16,
+    lineHeight: 24,
+  },
+});
